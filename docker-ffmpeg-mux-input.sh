aec5c820275e97a7dce301a538ac77999bbab1c7
visbits <tbishop@liquidweb.com>
2022-02-26 15:53:31 -0500

    Update docker-ffmpeg-mux-input.sh

--- docker-ffmpeg-mux-input.sh
+++ docker-ffmpeg-mux-input.sh
@@ -12,6 +12,7 @@ youtubeKey=$4
 password=$5
 bitrate=3400
 maxBitrate=4000
+mode=tcp
 
 docker rm -f $streamName
 docker run --name $streamName  --restart unless-stopped -d --network host \
@@ -23,25 +24,23 @@ linuxserver/ffmpeg \
 -loglevel warning \
 -err_detect explode \
 -abort_on empty_output_stream \
--rtsp_transport udp -max_delay 3000000 -reorder_queue_size 30000 -thread_queue_size 4096 -stimeout 4000000 \
+-rtsp_transport $mode -max_delay 3000000 -reorder_queue_size 30000 -thread_queue_size 4096 -stimeout 4000000 \
 -i $videoUrl"?overlays=all&audio=0&video=1&resolution=1920x1080&compression=30&videocodec=h264&h264profile=main&fps=30&videokeyframeinterval=60&videobitrate=$bitrate&videomaxbitrate=$maxBitrate" \
--rtsp_transport udp -max_delay 3000000 -reorder_queue_size 30000 -thread_queue_size 4096 -stimeout 4000000 \
+-rtsp_transport $mode -max_delay 3000000 -reorder_queue_size 30000 -thread_queue_size 4096 -stimeout 4000000 \
 -i $audioUrl"?audio=1&video=0" \
--f fifo -fifo_format flv \
+-f flv \
 -c:v copy \
 -c:a aac \
 -ar 44100 \
 -ab 128k \
 -af "aresample=async=1" \
 -map 0:v:0 -map 1:a:0 \
--drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 5 -restart_with_keyframe 1 -recover_any_error 1 \
 rtmp://localhost:1935/static/$streamName?password=$password \
--f fifo -fifo_format flv \
+-f flv \
 -c:v copy \
 -c:a aac \
 -ar 44100 \
 -ab 128k \
 -af "aresample=async=1" \
 -map 0:v:0 -map 1:a:0 \
--drop_pkts_on_overflow 1 -attempt_recovery 1 -recovery_wait_time 5 -restart_with_keyframe 1 -recover_any_error 1 \
 rtmps://a.rtmp.youtube.com/live2/$youtubeKey
